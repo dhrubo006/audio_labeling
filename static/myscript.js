091da7184e4e640bc38b2065a8d20d799ddf69dc
dhrubo006 <dhrubo006@gmail.com>
2023-10-23 18:43:54 +0600

    Feat: shows yes and no button when clicked submit Bug: the yes button works and submits the form but no button doesnt cancel submission

--- static/myscript.js
+++ static/myscript.js
@@ -10,5 +10,62 @@ function getRandomLabel() {
         .catch(error => console.error('Error:', error));
 }
 
+
+
+
+// Function to show the confirmation dialog
+function showConfirmation() {
+    const confirmYes = document.getElementById('confirmYes');
+    const confirmNo = document.getElementById('confirmNo');
+    const submitButton = document.getElementById('submitButton');
+
+    // Show confirmation buttons
+    confirmYes.style.display = 'inline-block';
+    confirmNo.style.display = 'inline-block';
+
+    // Disable the original submit button
+    submitButton.disabled = true;
+}
+
+
+// Function to handle the confirmation
+function handleConfirmation() {
+    const labelForm = document.getElementById('labelForm');
+    const confirmYes = document.getElementById('confirmYes');
+    const confirmNo = document.getElementById('confirmNo');
+
+    // Show confirmation buttons
+    confirmYes.style.display = 'inline-block';
+    confirmNo.style.display = 'inline-block';
+
+    // Disable the original submit button
+    const submitButton = document.getElementById('submitButton');
+    submitButton.disabled = true;
+
+    // Handle "Yes" click
+    confirmYes.addEventListener('click', function () {
+        // You can add code here to proceed with the submission
+        labelForm.submit(); // This submits the form
+    });
+
+    // Handle "No" click
+    confirmNo.addEventListener('click', function () {
+        // User canceled the submission, so hide the confirmation buttons
+        confirmYes.style.display = 'none';
+        confirmNo.style.display = 'none';
+
+        // Re-enable the original submit button
+        submitButton.disabled = false;
+    });
+}
+
+// Attach the handleConfirmation function to the form submission
+const labelForm = document.getElementById('labelForm');
+labelForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the default form submission
+    handleConfirmation();
+});
+
+
 // Call the function to fetch and display a random label
 getRandomLabel();
